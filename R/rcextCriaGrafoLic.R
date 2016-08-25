49661a8084ab00ac423e38c6de0745eda226c5a0
Bruno M S S Melo <ctbrunomelo@gmail.com>
2016-08-25 01:45:00 -0300

    rcextCriaGrafoLic - inclusao do desconto ofertado na criacao do grafo de licitacoes.

--- R/rcextCriaGrafoLic.R
+++ R/rcextCriaGrafoLic.R
@@ -8,8 +8,11 @@
 #'          relacoes do tipo “perdedor-vencedor”. Tal relacao e representada por uma aresta
 #'          que se inicia em no representativo da empresa participante perdedora para um no
 #'          representativo da licitante vencedora.
+#'         \item o desconto ofertado (diferenca entre o valor estimado e o valor homologado)
+#'         podera influenciar, de forma inversamente proporcional, o peso das relacoes
+#'         perdedor-vencedor. Quanto menor o desconto ofertado pelo vencedor, maior sera o peso
+#'         da referida relacao.
 #' }
-#'
 #' @param dados data.frame contendo as seguintes colunas:
 #' \itemize{
 #'         \item CNPJ coluna do tipo \code{character} contendo cnpj, com 14 caracteres (sem .,-, ou /),
@@ -19,6 +22,11 @@
 #'         a empresa esteja concorrendo. Caso o objeto da licitacao nao tenha sido dividido em itens, este campo
 #'         \item VENCEDOR coluna do tipo \code{logical} contendo um valor booleano indicando se o licitante foi
 #'         vitorioso no certame.
+#'         \item VALOR_ESTIMADO coluna do tipo \code{numeric} correspondente ao valor estimado para o objeto ou
+#'         serviço sendo licitado. Podera assumir o valor NA caso tal informacao nai esteja disponivel.
+#'         \item VALOR_HOMOLOGADO coluna do tipo \code{numeric} correspondente ao valor homologado da proposta
+#'         vencedora para o fornecimento do objeto ou serviço sendo licitado. Podera assumir o valor NA caso tal
+#'         informacao nai esteja disponivel.
 #' }
 #' @param tipo_retorno especifica o objeto a ser retornado pela funcao. As opcoes sao as que se seguem:
 #' \itemize{
@@ -29,18 +37,21 @@
 #'         \item 2 retorna um objeto do tipo \code{data.frame} a partir do qual poderá ser criado um grafo
 #'          por meio da funcao \code{igraph::graph.data.frame()}
 #'          }
+#' @param considerar_desconto parametro do tipo \code{logical} indicando se o desconto obtido (diferenca entre o valor
+#' homologado e o valor estimado) devera ser levado em consideracao na atribuicao dos pesos das relacoes perdedor-vencedor.
+#' Por padrao este parametro tem valor \code{TRUE}
 #' @return o retorno depende do valor especificado para o parâmetro \code{tipo_retorno}.
 #' @author Bruno M. S. S. Melo
 #' @examples
 #' \dontrun{
-#' grafoLic <- rcextCriaGrafoLic(dados = dfDadosLic, tipo_retorno = 0)
+#' grafoLic <- rcextCriaGrafoLic(dados = dfDadosLic, tipo_retorno = 0, considerar_desconto = F)
 #' }
 #' @seealso \code{igraph}
 #' @export
-rcextCriaGrafoLic <- function(dados, tipo_retorno = 0) {
+rcextCriaGrafoLic <- function(dados, tipo_retorno = 0, considerar_desconto = T) {
 
   if ((!is.numeric(tipo_retorno)) | (!(tipo_retorno %in% 0:2))) {
-      tipo_retorno <- 0
+    tipo_retorno <- 0
   }
 
   dfPERDEDOR <- subset(x = dados, VENCEDOR == F)
@@ -53,7 +64,9 @@ rcextCriaGrafoLic <- function(dados, tipo_retorno = 0) {
           dfPERDEDOR.CNPJ CNPJ_PERDEDOR,
           dfVENCEDOR.CNPJ CNPJ_VENCEDOR,
           dfVENCEDOR.ID_LICITACAO,
-          dfVENCEDOR.ID_ITEM
+          dfVENCEDOR.ID_ITEM,
+          dfVENCEDOR.VALOR_ESTIMADO,
+          dfVENCEDOR.VALOR_HOMOLOGADO
       FROM
           dfVENCEDOR
       INNER JOIN
@@ -63,8 +76,30 @@ rcextCriaGrafoLic <- function(dados, tipo_retorno = 0) {
                 AND dfVENCEDOR.CNPJ != dfPERDEDOR.CNPJ )'
   )
 
-  envGrafo$igGrafo <- igraph::graph.data.frame(envGrafo$dfGrafo)
+  if (considerar_desconto){
+    # retira registros que nao possaum valores estimados e valores homologados
+    envGrafo$dfGrafo <- envGrafo$dfGrafo[complete.cases(envGrafo$dfGrafo[, c("VALOR_ESTIMADO", "VALOR_HOMOLOGADO")]),]
 
+    # calcula peso da relacao perdedor-vencedor
+    envGrafo$dfGrafo$PESO_RELACAO <- envGrafo$dfGrafo$VALOR_HOMOLOGADO/envGrafo$dfGrafo$VALOR_ESTIMADO
+
+    # retira registros cujos pesos calculados sejam outliers
+    tukey <- fivenum(envGrafo$dfGrafo$PESO_RELACAO)
+    Q3 <- tukey[4]
+    Q1 <- tukey[2]
+    envGrafo$dfGrafo <- envGrafo$dfGrafo[envGrafo$dfGrafo$PESO_RELACAO < (Q3 + 1.5*(Q3-Q1)),]
+
+  } else{
+    envGrafo$dfGrafo$PESO_RELACAO <- 1
+  }
+
+  envGrafo$igGrafo <- igraph::graph.data.frame(
+    data.frame(
+      from = envGrafo$dfGrafo[, "CNPJ_PERDEDOR"],
+      to = envGrafo$dfGrafo[, "CNPJ_VENCEDOR"],
+      weight = envGrafo$dfGrafo[, "PESO_RELACAO"]
+    )
+  )
 
   return(envGrafo)
 }
